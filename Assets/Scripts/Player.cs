80e9151755d2eb0f773fd7b4d086c0f0a8311f64
t-kazuki <kazuki392.19961122@gmail.com>
2017-08-20 16:32:22 +0900

    chapter-6

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,9 @@ public class Player : MonoBehaviour
 
 			// 弾をプレイヤーと同じ位置/角度で作成
 			spaceship.Shot (transform);
-			
-			// 0.05秒待つ
-			yield return new WaitForSeconds (0.05f);
+
+			// shotDelay秒待つ
+			yield return new WaitForSeconds (spaceship.shotDelay);
 		}
 	}
 	
@@ -34,5 +34,29 @@ public class Player : MonoBehaviour
 		
 		// 移動
 		spaceship.Move (direction);
+	}
+	
+	// ぶつかった瞬間に呼び出される
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		// レイヤー名を取得
+		string layerName = LayerMask.LayerToName(c.gameObject.layer);
+		
+		// レイヤー名がBullet (Enemy)の時は弾を削除
+		if( layerName == "Bullet (Enemy)")
+		{
+			// 弾の削除
+			Destroy(c.gameObject);
+		}
+
+		// レイヤー名がBullet (Enemy)またはEnemyの場合は爆発
+		if( layerName == "Bullet (Enemy)" || layerName == "Enemy")
+		{
+			// 爆発する
+			spaceship.Explosion();
+		
+			// プレイヤーを削除
+			Destroy (gameObject);
+		}
 	}
 }
